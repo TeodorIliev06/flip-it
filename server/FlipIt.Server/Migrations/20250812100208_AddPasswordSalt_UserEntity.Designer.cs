768d34a66490bb021bf59bd201e5b47a36a5f461
TeodorIliev06 <tedo2505@gmail.com>
2025-08-12 13:06:25 +0300

    refactor(auth): switch to PBKDF2 password hashing

--- server/FlipIt.Server/Migrations/20250812100208_AddPasswordSalt_UserEntity.Designer.cs
+++ server/FlipIt.Server/Migrations/20250812100208_AddPasswordSalt_UserEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FlipIt.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FlipIt.Server.Migrations
 {
     [DbContext(typeof(FlipItDbContext))]
-    partial class FlipItDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250812100208_AddPasswordSalt_UserEntity")]
+    partial class AddPasswordSalt_UserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.12");
---
+++ server/FlipIt.Server/Migrations/20250812100208_AddPasswordSalt_UserEntity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlipIt.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordSalt_UserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiresAt",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiresAt",
+                table: "Users");
+        }
+    }
+}
