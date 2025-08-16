80cea76f10d1ca4ef18beea8935540565bfb7dc3
TeodorIliev06 <tedo2505@gmail.com>
2025-08-16 17:11:37 +0300

    feat: add username property

--- server/FlipIt.Server/Models/User.cs
+++ server/FlipIt.Server/Models/User.cs
@@ -3,12 +3,19 @@ namespace FlipIt.Server.Models;
 public class User
 {
     public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
     public string Email { get; set; } = null!;
+
     public string PasswordHash { get; set; } = null!;
+
     public string PasswordSalt { get; set; } = null!;
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public string? RefreshToken { get; set; }
+
     public DateTime? RefreshTokenExpiresAt { get; set; }
 
     public ICollection<Score> Scores { get; set; } = new List<Score>();
